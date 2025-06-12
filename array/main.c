aa3095e1d4d873fa4c564557d199389396f5bb9f
Laurens Southgate <laurenssouthgate@gmail.com>
2025-06-12 07:21:12 +0100

    reorganized array file

--- array/main.c
+++ array/main.c
@@ -1,6 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void printArray(int arr[], int n);
+void reversePrintArray(int arr[], int n);
+int returnMax(int arr[], int n);
+int returnSum(int arr[], int n);
+void reverseArray(int arr[], int n);
+void recursiveReverseArray(int arr[], int l, int r);
+void reverseArray2(int arr[], int n);
+void arrayInsert(int arr[], int *n, int pos, int val);
+void appendArray(int arr[], int *n, int val);
+void arrayDelete(int arr[], int *n, int key);
+void deleteLast(int arr[], int *n);
+
+
+int main(void)
+{
+    int arr[5] = {2, 4, 8, 16, 32};
+
+    printf("%d ", arr[2]);
+    printf("%d ", arr[4]);
+    printf("%d ", arr[0]);
+
+    arr[0] = 1;
+
+    printf("%d ", arr[0]);
+
+    arr[0] = 2;
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("\nSize of Array: %d\n", size);
+    printf("\n");
+
+    printArray(arr, size);
+    printf("\n");
+
+    reversePrintArray(arr, size);
+    printf("\n");
+
+    int arr2[] = {12, 4394, 934, 342, 432, 3, 4};
+    int len = sizeof(arr2) / sizeof(arr2[0]);
+    int max = returnMax(arr2, len);
+    printf("Max: %d\n", max);
+
+    int sum = returnSum(arr2, len);
+    printf("Sum: %d\n", sum);
+
+    recursiveReverseArray(arr2, 0, len - 1);
+    printArray(arr2, len);
+
+    reverseArray2(arr2, len);
+    printArray(arr2, len);
+
+    int arr3[7] = {10, 20, 30, 40, 50};
+    int n = 5;
+    int pos = 3;
+    int val = 25;
+
+    arrayInsert(arr3, &n, pos, val);
+    printArray(arr3, 7);
+
+    appendArray(arr3, &n, 60);
+    printArray(arr3, 7);
+
+    return 0;
+}
+
 void printArray(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
@@ -122,55 +188,8 @@ void arrayDelete(int arr[], int *n, int key)
     (*n)--;
 }
 
-int main(void)
+void deleteLast(int arr[], int *n)
 {
-    int arr[5] = {2, 4, 8, 16, 32};
-
-    printf("%d ", arr[2]);
-    printf("%d ", arr[4]);
-    printf("%d ", arr[0]);
-
-    arr[0] = 1;
-
-    printf("%d ", arr[0]);
-
-    arr[0] = 2;
-
-    int size = sizeof(arr) / sizeof(arr[0]);
-
-    printf("\nSize of Array: %d\n", size);
-    printf("\n");
-
-    printArray(arr, size);
-    printf("\n");
-
-    reversePrintArray(arr, size);
-    printf("\n");
-
-    int arr2[] = {12, 4394, 934, 342, 432, 3, 4};
-    int len = sizeof(arr2) / sizeof(arr2[0]);
-    int max = returnMax(arr2, len);
-    printf("Max: %d\n", max);
-
-    int sum = returnSum(arr2, len);
-    printf("Sum: %d\n", sum);
-
-    recursiveReverseArray(arr2, 0, len - 1);
-    printArray(arr2, len);
-
-    reverseArray2(arr2, len);
-    printArray(arr2, len);
-
-    int arr3[7] = {10, 20, 30, 40, 50};
-    int n = 5;
-    int pos = 3;
-    int val = 25;
-
-    arrayInsert(arr3, &n, pos, val);
-    printArray(arr3, 7);
-
-    appendArray(arr3, &n, 60);
-    printArray(arr3, 7);
-
-    return 0;
+	(*n)--;
 }
+
