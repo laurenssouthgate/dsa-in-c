84b64bbdb7e2127ad7654e491317f5a513e2a75d
Laurens Southgate <laurenssouthgate@gmail.com>
2025-06-14 08:23:22 +0100

    functions

--- functions/main.c
+++ functions/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 void printVal(int num, float real);
+void recursiveFn(int n);
 
 int main(void) {
 	int a = 3;
 
 	printVal(a, 1.5);
+
+	recursiveFn(a);
 
 	return 0;
 }
@@ -13,4 +16,12 @@ int main(void) {
 
 void printVal(int num, float real){
 	printf("%d %f\n", num, real);
+}
+
+void recursiveFn(int n) {
+	if (n == 6) return;
+
+	printf("Recursion level: %d", n);
+
+	recursiveFn(n + 1);
 }
